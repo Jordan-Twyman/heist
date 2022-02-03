74888d99ae9ab27629ad151ec3709656ed3d86c7
Jaytwy24 <jtwy24@gmail.com>
2022-02-03 13:33:37 -0500

    heist part 1

--- Program.cs
+++ Program.cs
@@ -2,121 +2,86 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Heist
+namespace heist
 {
-  public class Bank
-  {
-    public Bank()
+    class Program
     {
-      //? This diffLevel will start the loop 
-      int diffLevel = 0;
-      while (diffLevel <= 0)
-      {
-        //? Try to get the input. It will only really throw if the string cannot be converted to integer.
-        //? A negative number can be input but it will not cause an exception. 
-        //? When there is an exception just skip current iteration and reprompt user. 
-        try
+        static void Main(string[] args)
         {
-          Console.WriteLine("Enter a difficulty level for the bank, must be a positive integer");
-          diffLevel = Int32.Parse(Console.ReadLine());
-        }
-        catch (Exception)
-        {
-          continue;
-        }
-      }
+            List<TeamMember> Team = new List<TeamMember>();
+            TeamMember inputTeamMember = new TeamMember();
+            int TeamSkillTotal = 0;
+            int BankLevel = 0;
 
-      Difficulty = diffLevel;
+            Console.WriteLine("Plan your heist!");
+            Console.WriteLine("----------------");
+            Console.WriteLine("Please enter your bank difficulty level.");
+            BankLevel = Int32.Parse(Console.ReadLine());
+
+            //bool to cause while loop to break
+            bool AddTeamMate = true;
+            while (AddTeamMate)
+            {
+                //conditional
+                Console.WriteLine("What's the team member's name?");
+                inputTeamMember.Name = Console.ReadLine();
+
+                // while loop will continue until name is blank
+                if (inputTeamMember.Name != "")
+                {
+                    Console.WriteLine("What's the team member's skill level?");
+                    inputTeamMember.SkillLevel = Int32.Parse(Console.ReadLine());
+                    // Console.WriteLine("Please enter a skill level from 1-100");
+                    Console.WriteLine("What's team member's courage level? 0.0 - 2.0");
+                    inputTeamMember.CourageFactor = Double.Parse(Console.ReadLine());
+                    //Console.WriteLine("Please enter a decimal for your courage factor. 0.0 - 2.0");
+                    // Console.WriteLine($"Your team member's name is {inputTeamMember.Name}. They're skill level is {inputTeamMember.SkillLevel} and their courage level is {inputTeamMember.CourageFactor}");
+
+                    TeamMember newMember = new TeamMember()
+                    {
+                        Name = inputTeamMember.Name,
+                        SkillLevel = inputTeamMember.SkillLevel,
+                        CourageFactor = inputTeamMember.CourageFactor
+                    };
+                    Team.Add(newMember);
+                    inputTeamMember.SkillLevel = 0;
+                    int x = Team.Count;
+                    // Team.ForEach(member => Console.WriteLine($"# of Team Members: {x}"));
+                }
+                else
+                {
+                    Console.WriteLine("How many trial runs would you like to do?");
+                    string response = Console.ReadLine();
+                    int parsedResponse = Int32.Parse(response);
+                    int success = 0;
+                    int failure = 0;
+                    for (int i = 0; i < parsedResponse; i++)
+                    {
+                        int RandomNumber = new Random().Next(-10, 10);
+
+                        TeamSkillTotal = Team.Sum(member => member.SkillLevel);
+                        if (TeamSkillTotal + RandomNumber > BankLevel)
+                        {
+                            success++;
+                        }
+                        else
+                        {
+                           failure++;
+                        }
+                    }
+                    //report Team vs Bank --- how many times succeeded how many times lost
+                    Console.WriteLine($@"
+                    Team Skill Level: {TeamSkillTotal}
+                    Bank Difficulty Level: {BankLevel}
+                    Successes: {success}
+                    Failures: {failure}");
+                    break;
+                }
+            }
+
+
+
+        }
+
     }
-    //* Store a value for the bank's difficulty level. Set this value to 100.
-    public int Difficulty { get; set; }
-  }
-  public class HeistReport
-  {
-    public int TeamSkillLevel { get; set; }
-    public int BankDifficultyLevel { get; set; }
-    public bool SuccessfulHeist { get; set; }
-  }
-
-  public class EndOfHeistsReport
-  {
-    public int SuccessfulHeistCount { get; set; }
-    public int FailedHeistCount { get; set; }
-  }
-  class Program
-  {
-
-    static void Main(string[] args)
-    {
-      Bank bank = new Bank();
-      
-      int bankDifficulty = bank.Difficulty;
-      //* Print the message "Plan Your Heist!".
-      Console.WriteLine("Plan Your Heist!");
-      Console.WriteLine("----------------");
-
-      Team newTeam = new Team();
-      
-      List<TeamMember> teamList = newTeam.GetTeamMembers();
-
-      int trialRuns = 0;
-
-      while (trialRuns <= 0)
-      {
-        try
-        {
-        
-          Console.WriteLine("------------------------------------------");
-
-          Console.WriteLine("Enter a number of trial runs for the team.");
-          trialRuns = Int32.Parse(Console.ReadLine());
-        }
-        catch (Exception)
-        {
-          Console.WriteLine("Enter a number of trial runs for the team. Must be a positive integer greater than 0..");
-          trialRuns = Int32.Parse(Console.ReadLine());
-        }
-      }
-      
-      //* Before displaying the success or failure message, display a report that shows.
-      //* The team's combined skill level
-      //* The bank's difficulty level
-      List<HeistReport> heistReports = new List<HeistReport>();
-      for (int i = trialRuns; i > 0; i--)
-      {
-        HeistReport report = new HeistReport {
-          TeamSkillLevel = teamList.Sum(tm => tm.GetSkillLevel()),
-          //* Create a random number between -10 and 10 for the heist's luck value.
-          //* Add this number to the bank's difficulty level.
-          BankDifficultyLevel = bankDifficulty + new Random().Next(-10, 10)
-        };
-        heistReports.Add(report);
-      }
-      
-      //* Compare the number with the bank's difficulty level.
-      //* If the team's skill level is greater than or equal to the bank's difficulty level,
-      //* Display a success message, otherwise display a failure message.
-      heistReports.ForEach(hr => {
-        if (hr.TeamSkillLevel >= hr.BankDifficultyLevel)
-        {
-          hr.SuccessfulHeist = true;
-        } 
-        else
-        {
-          hr.SuccessfulHeist = false;
-        }
-      });
-
-      EndOfHeistsReport endOfHeistsReport = new EndOfHeistsReport(){
-        SuccessfulHeistCount = heistReports.Select(hr => hr.SuccessfulHeist).Where(hrSuccess => hrSuccess == true).Count(),
-        FailedHeistCount = heistReports.Select(hr => hr.SuccessfulHeist).Where(hrSuccess => hrSuccess == false).Count()
-      };
-
-      Console.WriteLine(@$"
-      --End of Heists Report--
-        {endOfHeistsReport.SuccessfulHeistCount} heists were successful!
-        {endOfHeistsReport.FailedHeistCount} heists failed!
-      ");
-    }
-  }
 }
